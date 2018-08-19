2b4fcc4c639fb591c1e5dee21dfa003809100e57
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2018-08-19 10:56:11 +0200

    Created component ApplicationLogo.

---
+++ src/base-components/ApplicationLogo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app-logo">
+    <span class="app-logo__letter">H</span>
+    <span class="app-logo__letter">E</span>
+    <span class="app-logo__letter">Y</span>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.app-logo {
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+
+  &__letter {
+    border-radius: 100px;
+    width: 30px;
+    height: 30px;
+    font-family: 'Poppins', sans-serif;
+    font-weight: bold;
+    font-size: 20px;
+    color: #fff;
+
+    & + & {
+      margin-left: -9px;
+    }
+
+    &:first-child {
+      background-color: #4183D7;
+    }
+
+    &:nth-child(2) {
+      background-color: #F64747;
+    }
+
+    &:last-child {
+      background-color: #3FC380;
+    }
+  }
+}
+</style>
