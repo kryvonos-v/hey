0ec337cadc105e182b9586ecbf59af385703e47a
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2018-08-20 20:39:25 +0200

    Created new animation for MenuBurger.

--- src/components/ApplicationNavbar/components/MenuBurger.vue
+++ src/components/ApplicationNavbar/components/MenuBurger.vue
@@ -1,5 +1,13 @@
 <template>
-  <a role="button" class="menu-burger" aria-expanded="false">
+  <a
+    role="button"
+    :class="{
+      'menu-burger': true,
+      'menu-burger--active': opened
+    }"
+    :aria-expanded="opened"
+    v-on="$listeners"
+  >
     <span class="menu-burger__stick"></span>
     <span class="menu-burger__stick"></span>
     <span class="menu-burger__stick"></span>
@@ -10,7 +18,12 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-    
+  props: {
+    opened: {
+      type: Boolean,
+      default: false
+    }
+  }
 })
 </script>
 
@@ -18,34 +31,41 @@ export default Vue.extend({
 <style lang="scss">
 .menu-burger {
   position: relative;
-  width: 22px;
-  height: 12px;
+  width: 3.25rem;
+  height: 3.25rem;
   cursor: pointer;
 
   &__stick {
+    $stick-width: 22px;
+    $stick-height: 2px;
+
     display: block;
     position: absolute;
-    right: 0px;
-    width: 100%;
-    height: 2px;
+    left: calc(50% - #{$stick-width / 2});
+    right: auto;
+    width: $stick-width;
+    height: $stick-height;
     opacity: 1;
     background: #fff;
     transition: all .3s ease-in-out;
 
     &:first-child {
-      top: 0;
+      top: calc(50% - 6px);
       bottom: auto;
-      transform-origin: 6px 5px;
+      animation-name: move-and-rotate-top-stick-in;
+      animation-duration: 300ms;
+      animation-timing-function: ease-in-out;
+      animation-fill-mode: forwards;
 
       @at-root .menu-burger--active & {
-        transform: rotate(45deg);
-        top: 0px;
+        animation-name: move-and-rotate-top-stick-out;
       }
     }
 
     &:nth-child(2) {
       top: 50%;
       margin-top: -1px;
+      transition: opacity 200ms ease-in-out;
 
       @at-root .menu-burger--active & {
         opacity: 0;
@@ -53,15 +73,88 @@ export default Vue.extend({
     }
 
     &:last-child {
-      top: auto;
-      bottom: 0;
-      transform-origin: 5px -2px;
+      top: calc(50% + 4px);
+      animation-name: move-and-rotate-bottom-stick-in;
+      animation-duration: 300ms;
+      animation-timing-function: ease-in-out;
+      animation-fill-mode: forwards;
 
       @at-root .menu-burger--active & {
-        transform: rotate(-45deg);
-        bottom: 0px;
+        animation-name: move-and-rotate-bottom-stick-out;
       }
     }
+  }
+}
+
+@keyframes move-and-rotate-bottom-stick-in {
+  0% {
+    transform: translateY(-5px) rotate(-45deg);
+  }
+
+  40% {
+    transform: translateY(-5px);
+  }
+
+  60% {
+    transform: translateY(-5px);
+  }
+
+  100% {
+    transform: translateY(0) rotate(0deg);
+  }
+}
+
+@keyframes move-and-rotate-bottom-stick-out {
+  0% {
+    transform: translateY(0) rotate(0deg);
+  }
+
+  40% {
+    transform: translateY(-5px);
+  }
+
+  60% {
+    transform: translateY(-5px);
+  }
+
+  100% {
+    transform: translateY(-5px) rotate(-45deg);
+  }
+}
+
+@keyframes move-and-rotate-top-stick-in {
+  0% {
+    transform: translateY(5px) rotate(45deg);
+  }
+
+  40% {
+    transform: translateY(5px);
+  }
+
+  60% {
+    transform: translateY(5px);
+  }
+
+  100% {
+    transform: translateY(0) rotate(0deg);
+  }
+}
+
+@keyframes move-and-rotate-top-stick-out {
+  0% {
+    transform: translateY(0) rotate(0deg);
+  }
+
+  40% {
+    transform: translateY(5px);
+  }
+
+  60% {
+    transform: translateY(5px);
+  }
+
+  100% {
+    transform: translateY(5px) rotate(45deg);
   }
 }
 </style>
