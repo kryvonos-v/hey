f51d28c48adb80be116a4ba6607f3fc87edd197b
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2018-08-24 08:44:57 +0200

    Transforming api responses from snake to camel case.

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,12 +3,18 @@ import {
 } from '@/types/api'
 import * as endpoint from '@/shared/enums/endpoint'
 import axios from 'axios'
+import camelCaseKeys from 'camelcase-keys'
 
 let axiosMovieDB = axios.create({
   baseURL: endpoint.API_URL,
   params: {
     api_key: process.env.VUE_APP_MOVIE_DB_API_KEY
-  }
+  },
+  transformResponse: [(data, headers) => {
+    let dataObj = JSON.parse(data)
+
+    return camelCaseKeys(dataObj, { deep: true })
+  }]
 })
 
 export async function searchMovie (params: MovieSearchRequest) {
