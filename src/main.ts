0e6aab0a867627720e45418d1a5494d5183aad07
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2018-08-26 08:13:11 +0200

    Added fontawesome.

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/main.scss'
-import Buefy from 'buefy'
+import DropdownItem from 'buefy/src/components/dropdown/DropdownItem.vue'
 import clickOutside from './shared/directives/click-outside'
 import Dropdown from './components/Dropdown/Dropdown.vue'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faCoffee, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-let BuefyComponents = <any>Buefy
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.directive('click-outside', clickOutside)
 Vue.component('b-dropdown', Dropdown)
-Vue.component(BuefyComponents.DropdownItem.name, BuefyComponents.DropdownItem)
+Vue.component(DropdownItem.name, DropdownItem)
 
 Vue.config.productionTip = false
 
